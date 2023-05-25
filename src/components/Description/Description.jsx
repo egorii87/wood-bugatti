674a889489bcee9d18fba2d4c59965a9e1518eac
egorkalinin <yahor.kalinin@gmail.com>
2023-05-25 23:34:25 +0300

    Deleting text, changing logo

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -4,17 +4,8 @@ import logo from '../../assets/images/logo.jpg'
 
 export const Description = () => {
   return (
-    <div>
-      <img srcSet={logo} alt="logo" className='img-logo'/>
-      <div className="description">
-      
-      
-      <h1 className='description-header'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates!</h1>
-      <div className="description-paragraphs">
-        <p className="paragraphs-item">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab commodi pariatur assumenda distinctio, earum ex optio sint asperiores iste est eaque ratione consequatur vero doloribus, sequi illo inventore aut cupiditate suscipit, facere rem. Vel alias cumque quis nesciunt dolorum magni voluptatem, omnis eum earum natus expedita ducimus dolores incidunt pariatur.</p>
-        <p className="paragraphs-item">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste optio adipisci perferendis assumenda, laboriosam, a cumque iusto in explicabo necessitatibus quisquam nam magni mollitia dolorem quos tempore commodi eaque sequi ratione earum accusantium blanditiis. Nostrum porro aspernatur itaque animi nulla iure ipsa voluptatibus molestiae, omnis ipsum vitae velit at soluta.</p>
-      </div>
-    </div>
+    <div className='img-logo'>
+      <img srcSet={logo} alt="logo" className='img-logo-item'/>
     </div>
     
   )
